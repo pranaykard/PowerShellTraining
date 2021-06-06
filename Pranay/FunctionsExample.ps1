727d0938c0c329e452dc0b2b3b4512592969b434
unknown <pranay.kardile@gmail.com>
2021-06-06 11:18:19 +0530

    Adding new scripts

---
+++ Pranay/FunctionsExample.ps1
@@ -0,0 +1,26 @@
+﻿
+
+function GetServicesData ($ServiceName,$StartType) {
+
+$ServiceList = Get-Service -Name $ServiceName | where {$_.StartType -like "Manual"}
+
+
+    foreach ($Service in $ServiceList)
+
+    {
+
+        if ($StartType -eq "Automatic")
+
+        {Write-Host "Start type is $($Service.StartType)"}
+
+        elseif ($StartType -eq "Manual")
+
+        {Write-Host "Start type is $($Service.StartType)"}
+
+    }
+
+    return $ServiceList
+
+}
+
+$Service = GetServicesData -ServiceName "BITS" -StartType "Automatic" # Calling the function
