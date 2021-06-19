53bbafba15c45658de54b54443579d9e3b01d945
jagat <Jagat.pradhan@live.com>
2021-06-19 22:07:42 +0530

    pre_post_service_check_start_stopped_service week5

---
+++ Jagat/Week5/Pre_Post_Service_Check.ps1
@@ -0,0 +1,29 @@
+﻿$pre = Import-Csv C:\Temp\pre.csv
+$post = Import-Csv C:\Temp\post.csv
+
+
+$diff = Compare-Object -ReferenceObject $pre -DifferenceObject $post -Property Name,Status | Where-Object {$_.Status -eq "Stopped"}
+$diff | Export-Csv C:\Temp\diff.csv -NoTypeInformation
+
+$import_svc = Import-Csv C:\Temp\diff.csv | select -ExpandProperty Name
+
+
+foreach($service in $import_svc)
+
+{
+ 
+        Start-Service $service
+        Write-Host "$($service) was stopped after reboot, now Started" -ForegroundColor Green
+
+     
+
+}
+
+#clean up
+Remove-Item C:\Temp\diff.csv
+Write-Host "C:\Temp\diff.csv  Deleted" -ForegroundColor Yellow
+
+
+
+
+
